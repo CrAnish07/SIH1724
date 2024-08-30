72c46ab6b7502c7e16baf4f3a06976d844eb436d
Anish Mondal <mondalanish10@gmail.com>
2024-08-30 13:06:55 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React, {useState, useEffect} from "react"
+import './style.css'
+
+
+const App = () => {
+  const [htmlContent, setHtmlContent] = useState('')
+
+  useEffect(()=>{
+    fetch('/src/dashboard.html')
+      .then(response => response.text())
+      .then(data => setHtmlContent(data))
+  }, []);
+
+  return(
+    <div dangerouslySetInnerHTML={{__html: htmlContent}}/>
+  )
+}
+
+export default App
