ceb010632d63411779f8592778d7d7c682427a92
Anish Mondal <mondalanish10@gmail.com>
2024-09-14 21:24:45 +0530

    end off commit

--- src/DiscussForum/mainForum.jsx
+++ src/DiscussForum/mainForum.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 
 import App from './forum';
 
-document.getElementsByClassName('nav-item4').addEventListener('click', () => {
-    ReactDOM.render(<App />, document.getElementById('root'));
-  });
+// document.getElementsByClassName('nav-item4').addEventListener('click', () => {
+//     ReactDOM.render(<App />, document.getElementById('root'));
+//   });
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
